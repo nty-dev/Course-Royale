f05d93371eccbaddd98d77d708528fa48665d8c5
Neo Tsay Yong <padricneo@gmail.com>
2025-01-18 16:17:42 +0800

    updated footer

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,15 +5,22 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a
+          href="https://github.com/nty-dev/Course-Royale"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CourseRoyale@Edurec
         </a>
-        <span className="ms-1">&copy; 2024 creativeLabs.</span>
       </div>
       <div className="ms-auto">
         <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
+        <a
+          href="https://github.com/nty-dev/Course-Royale"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Course Royale Development Team
         </a>
       </div>
     </CFooter>
